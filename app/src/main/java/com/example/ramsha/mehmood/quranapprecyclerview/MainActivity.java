d1e72d2da8fc934d05a73dbfffb57b02cf0aa40b
RSoft456 <ramshamehmood456@gmail.com>
2022-09-04 02:40:44 +0500

    NavigationDrawer added

---
+++ app/src/main/java/com/example/ramsha/mehmood/quranapprecyclerview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.ramsha.mehmood.quranapprecyclerview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
